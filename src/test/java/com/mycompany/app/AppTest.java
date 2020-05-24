9041cfd365cb9d2e21ffb97932e37a211de857ee
Ubuntu <ubuntu@ip-172-31-25-111.ap-south-1.compute.internal>
2020-05-24 07:24:24 +0000

    Add new feature

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -8,13 +8,22 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest 
-{
+{ 
+
+    public boolean isPositiveNumber(int num) {
+ 	if( num >= 0) {
+		return true;	
+	} else {
+		return false;
+	}
+    }
    /**
      * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        boolean result = isPositiveNumber(-10);
+        assertTrue( result);
     }
 }
